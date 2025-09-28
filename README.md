efb6cd483144c9d801164b46ffa4c874b89798b5
Patrik Fiala <patrikfiala@Patrik-MacBook-Air.local>
2025-09-28 11:53:38 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# OECD-GINI
+This beginner project aims to predict the Gini index of countries based on factors such as primary healthcare spending and unemployment, using linear regression as the model. I have used open datasets from OECD (oecd.org), which are included in the data folder.
+
+The model is not intended for actual prediction, but to showcase data cleaning, feature engineering, and baseline prediction using linear regression.
+
+Technologies used in this project:1.Python
+                                  2.pandas
+                                  3.scikit-learn (LinearRegression, OneHotEncoder, SimpleImputer, ColumnTransformer, Pipeline)
+
+Limitations: the dataset is small (~90 rows) and only a few features were used, resulting in relatively low predictive accuracy.
---
+++ main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+idd = pd.read_csv('data/IDD.csv')
+health = pd.read_csv('data/health.csv')
+unemployment = pd.read_csv('data/unemployment.csv')
+
+for df in [idd, health, unemployment]:
+    df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+
+idd.TIME_PERIOD = idd.TIME_PERIOD.astype(int)
+health.TIME_PERIOD = health.TIME_PERIOD.astype(int)
+unemployment.TIME_PERIOD = unemployment.TIME_PERIOD.astype(int)
+
+df = pd.merge(idd ,health, on=['REF_AREA', 'TIME_PERIOD'], how='inner')
+df = pd.merge(df ,unemployment, on=['REF_AREA', 'TIME_PERIOD'], how='inner')
+
+means = df.groupby(['REF_AREA', 'TIME_PERIOD'], as_index=False)['OBS_VALUE'].mean()
+df = means.merge(df.drop(columns=['OBS_VALUE']).drop_duplicates(['REF_AREA','TIME_PERIOD']),
+                 on=['REF_AREA','TIME_PERIOD'],
+                 how='left')
+
+features = ['OBS_VALUE_x', 'OBS_VALUE_y', 'OBS_VALUE', 'LABOUR_FORCE_STATUS']
+df = df[features]
+X = df.drop(columns=['OBS_VALUE_x'])
+y = df['OBS_VALUE_x']
+
+
+
+preprocessor = ColumnTransformer(transformers = [('num', SimpleImputer(strategy='mean'), ['OBS_VALUE', 'OBS_VALUE_y']), ('cat', OneHotEncoder(handle_unknown='ignore'), ['LABOUR_FORCE_STATUS'])])
+
+reg = Pipeline(steps=[('preprocessor', preprocessor), ('regressor', LinearRegression())])
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
+
+reg.fit(X_train, y_train)
+
+print(f"Model R^2: {reg.score(X_valid, y_valid)}")
